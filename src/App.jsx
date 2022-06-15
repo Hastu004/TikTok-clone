eb062250fd8aca39e33b0ec06a1e72401668e7af
Hastu004 <hernan.astudillo.r@gmail.com>
2022-06-14 23:04:42 -0400

    feat(root): TikTok core functional with Sepabase and hooks

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import FeedVideos from "./components/FeedVideos/index.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        {" "}
+        <FeedVideos />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VideoPlayer/index.jsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import { useRef, useState, useEffect } from "react";
+import styles from "./styles.module.css";
+import VideoPlayerActions from "./VideoPlayerActions.jsx";
+import VideoDescription from "../VideoDescription/index.jsx";
+import useIntersectionVideoPlayer from '../../hooks/useIntersectionVideoPlayer.js'
+
+
+export default function VideoPlayer(props) {
+  const video = useRef(null);
+  const {playing, handlePlay} = useIntersectionVideoPlayer({video})
+
+
+
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing,
+  });
+
+  const { src } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <video
+        className={styles.video}
+        controls={false}
+        loop
+        onClick={handlePlay}
+        ref={video}
+        src={src}
+      />
+      <i className={playerClassName} onClick={handlePlay} />
+      <VideoPlayerActions {...props} />
+      <VideoDescription {...props} />
+    </div>
+  );
+}
